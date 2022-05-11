488fd15640d50ae1e61a0d4bec53fdc4d3046835
Cunliang Geng <c.geng@esciencecenter.nl>
2022-05-11 14:31:20 +0200

    Update start_fhir_server.sh

--- start_fhir_server.sh
+++ start_fhir_server.sh
@@ -7,7 +7,7 @@ if [ $# != 1 ]; then
 else
     mvn -D="jna.library.path=/usr/local/lib" \
         -Dgb.url=$1 \
-        -Dgwc.player=/mapping_configs/gamebus_fhir_r4/configurations/player.textproto \
-        -Dgwc.activity=/mapping_configs/gamebus_fhir_r4/configurations/activity.textproto \
+        -Dgwc.player="/mapping_configs/gamebus_fhir_r4/configurations/player.textproto" \
+        -Dgwc.activity="/mapping_configs/gamebus_fhir_r4/configurations/activity.textproto" \
         jetty:run
 fi
